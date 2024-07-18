8e46e2701989bd0021daab80e7e9f078b8641baa
AnshulAnand1 <dev.anshul.anand@gmail.com>
2024-07-18 20:49:27 +0530

    added account schema in db.js added user search in ./routs/user.js in router.get("/bulk

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,46 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://devanshulanand:xQgp8f13CenQfaIZ@cluster0.hqq1b6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 });
+
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
 
 const User = mongoose.model("User", userSchema);
 
